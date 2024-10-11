4c0b2465549b49c9d3af107f258b4fd5ac6211c0
evezhinin <evezhinin@gmail.com>
2024-10-11 10:21:57 -0400

    Added a boiler plate for HomePage

---
+++ src/Components/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './HomePage.css';
+
+const HomePage = () => {
+    //adding functionality to code goes here
+    return (
+        //html code goes here
+        <div className="homepage"></div>
+    )
+
+}
+export default HomePage;
