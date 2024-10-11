d013ed5d94d8dda2963ccdd0bebe23358f2a7438
Chelsea <owner@Chelseas-MacBook-Pro.local>
2024-10-11 12:44:29 -0400

    lines of code for footer

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    //adding functionality to code goes here
+    return (
+        //html code goes here
+       <div></div>
+    )
+
+}
+export default Footer;
