586c54773008dfd724a375518d8003a18e65a114
evezhinin <evezhinin@gmail.com>
2024-10-11 10:05:02 -0400

    Added a boilerplate for Homepage

---
+++ src/Components/HomePage/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './HomePage.css';
+
+const HomePage = ()=>{
+    return(
+        //here goes html code
+        <div className='HomePage'></div>
+
+    );
+}
+export default HomePage;
