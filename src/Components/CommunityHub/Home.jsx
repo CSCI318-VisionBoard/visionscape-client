f5b5571c7f97cb37b40b664d6f7b0fce42574660
Neal <nealbbayla@gmail.com>
2024-11-22 09:19:59 -0500

    Community Hub

---
+++ src/Components/CommunityHub/Home.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import Nav from "./Nav";
+
+const Home = () => {
+    const [thread, setThread] = useState("");
+
+    const [threadList, setThreadList] = useState([]);
+
+    const createThread = () => {
+    fetch("http://localhost:4000/api/create/thread", {
+        method: "POST",
+        body: JSON.stringify({
+            thread,
+            userId: localStorage.getItem("_id"),
+        }),
+        headers: {
+            "Content-Type": "application/json",
+        },
+    })
+        .then((res) => res.json())
+        .then((data) => {
+            alert(data.message);
+            setThreadList(data.threads);
+        })
+        .catch((err) => console.error(err));
+};
+    
+    //👇🏻 Triggered when the form is submitted
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        //👇🏻 Calls the function
+        createThread();
+        setThread("");
+    };
+
+    const navigate = useNavigate();
+
+    //👇🏻 The useEffect Hook
+    useEffect(() => {
+        const checkUser = () => {
+            if (!localStorage.getItem("_id")) {
+                navigate("/");
+            } else {
+                fetch("http://localhost:4000/api/all/threads")
+                    .then((res) => res.json())
+                    .then((data) => setThreadList(data.threads))
+                    .catch((err) => console.error(err));
+            }
+        };
+        checkUser();
+    }, [navigate]);
+
+    return (
+        <>
+            <Nav />
+            <main className='home'>
+                <h2 className='homeTitle'>Create a Thread</h2>
+                <form className='homeForm' onSubmit={handleSubmit}>
+                    {/*--form UI elements--*/}
+                </form>
+    
+                <div className='thread__container'>
+                    {threadList.map((thread) => (
+                        <div className='thread__item' key={thread.id}>
+                            <p>{thread.title}</p>
+                            <div className='react__container'>
+                                <Likes numberOfLikes={thread.likes.length} threadId={thread.id} />
+                                <Comments
+                                    numberOfComments={thread.replies.length}
+                                    threadId={thread.id}
+                                    title={thread.title}
+                                />
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </main>
+        </>
+    );
+};
+
+export default Home;
---
+++ src/Components/CommunityHub/Nav.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Nav = () => {
+    const signOut = () => {
+        localStorage.removeItem("_id");
+        //👇🏻 redirects to the login page
+        navigate("/");
+    };
+
+    return (
+        <nav className='navbar'>
+            <h2>Threadify</h2>
+            <div className='navbarRight'>
+                <button onClick={signOut}>Sign out</button>
+            </div>
+        </nav>
+    );
+};
+
+export default Nav;
